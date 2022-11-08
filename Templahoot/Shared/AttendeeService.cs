362c140149fd26f52a93fcb97d05e88c6fe8efdf
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2022-11-08 15:36:44 +0100

    Allow 1 character names

--- Templahoot/Shared/AttendeeService.cs
+++ Templahoot/Shared/AttendeeService.cs
@@ -4,13 +4,6 @@ namespace Templahoot.Shared;
 
 public class AttendeeService
 {
-    private readonly CircuitTracker _circuitTracker;
-
-    public AttendeeService(CircuitTracker circuitTracker)
-    {
-        _circuitTracker = circuitTracker;
-    }
-
     public string Name { get; set; }
     public string CircuitId { get; set; }
     public bool IsNameSet { get; set; }
